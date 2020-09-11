23cce97352b79ef8be9f3bddf3a9625fb2e2789a
Medwed-ops <lesuus93@gmail.com>
2020-09-11 18:00:18 +0300

    Initial commit

---
+++ src/task1/TestAuthor.java
@@ -0,0 +1,10 @@
+package task1;
+
+public class TestAuthor
+{
+    public static void main(String[] args)
+    {
+        Author a1 = new Author("Alex","alex@gmail.com",'M');
+        System.out.println(a1.toString());
+    }
+}
